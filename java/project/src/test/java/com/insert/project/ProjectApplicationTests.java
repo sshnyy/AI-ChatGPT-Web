14108f5507eebc747b97bfb25fd9405b05fc16e4
Sohyeon Yun <99328827+sshnyy@users.noreply.github.com>
2024-07-05 12:26:24 +0900

    Create ProjectApplicationTests.java

---
+++ java/project/src/test/java/com/insert/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insert.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
